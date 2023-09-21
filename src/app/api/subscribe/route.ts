f9d85a4cd73386cc14210465591f9928c6ea9d56
safethecode <iam@sonjim.in>
2023-09-21 12:07:07 +0900

    feat(api/subscribe): 데이터베이스에 유저 정보 insert 기능 추가

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { supabase } from '@/utils/supabase';
 
 export async function POST(request: NextRequest) {
   const SLACK_WEBHOOK_URL =
@@ -8,8 +9,22 @@ export async function POST(request: NextRequest) {
 
   const content = await fetch(SLACK_WEBHOOK_URL, {
     method: 'POST',
-    body: JSON.stringify(requestBody),
+    body: JSON.stringify(requestBody.message),
   });
+
+  const { data, error } = await supabase
+    .from('subscribe_users')
+    .insert([
+      {
+        email: requestBody.userInfo.email,
+        name: requestBody.userInfo.name,
+        privacy_policy_agreed: true,
+        terms_of_service_agreed: true,
+      },
+    ])
+    .select();
+
+  console.log(data, error);
 
   return NextResponse.json(content.json());
 }
