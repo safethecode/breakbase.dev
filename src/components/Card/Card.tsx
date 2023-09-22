916aeb4e8a04865111e2541e7c3f4e50a6786011
safethecode <iam@sonjim.in>
2023-09-22 18:38:01 +0900

    feat(components/Card): 커밍순 대신 월요일 카운터 추가

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,11 @@
+import { MondayCount } from '..';
 import * as style from './Card.css';
 
 export const Card = ({ onClick }: { onClick?: () => void }) => {
   return (
     <article className={style.wrap} onClick={onClick}>
       <span className={style.cardLabel}>첫번째 뉴스레터</span>
-      <h3 className={style.comingSoon}>Coming Soon</h3>
+      <MondayCount />
     </article>
   );
 };
