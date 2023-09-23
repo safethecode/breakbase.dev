7a4ea63499950f026d96b709698ca6ed4bd11b01
safethecode <iam@sonjim.in>
2023-09-23 12:45:39 +0900

    fix(robots): sitemap 동적 hostname 제거

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,15 +1,13 @@
 import { MetadataRoute } from 'next';
-import { headersList } from '@/utils';
 
 export default function robots(): MetadataRoute.Robots {
-  const hostName = headersList.get('Host') as string;
-
   return {
     rules: {
       userAgent: '*',
       disallow: '/_next/',
       allow: '/',
     },
-    sitemap: `https://${hostName}/sitemap.xml`,
+    sitemap: `https://fitzza.xyz/sitemap.xml`,
+    host: `https://fitzza.xyz`,
   };
 }
