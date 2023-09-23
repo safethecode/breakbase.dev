4dadaab0faa52f50e13f9c7c4d9b885ad1633760
safethecode <iam@sonjim.in>
2023-09-23 12:07:28 +0900

    feat(components/Card): 변경된 카윤트다운 컴포넌트 적용

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,11 +1,15 @@
-import { MondayCount } from '..';
+import { CountDown } from '..';
 import * as style from './Card.css';
 
-export const Card = ({ onClick }: { onClick?: () => void }) => {
+interface CardProps {
+  onClick?: () => void;
+}
+
+export const Card = ({ onClick }: CardProps) => {
   return (
     <article className={style.wrap} onClick={onClick}>
       <span className={style.cardLabel}>첫번째 뉴스레터</span>
-      <MondayCount />
+      <CountDown />
     </article>
   );
 };
