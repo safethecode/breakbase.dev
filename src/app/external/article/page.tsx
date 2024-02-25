6d9153504163311a09911be7161366a22ebfc1a1
safethecode <iam@sonjim.in>
2024-02-25 21:49:44 +0900

    feat(external): 외부에게 공개되는 기본 베이스 페이지 상태로 추가

---
+++ src/app/external/article/page.tsx
@@ -0,0 +1,9 @@
+const ExternalArticlePage = () => {
+  return (
+    <div>
+      <h1>External Article Page</h1>
+    </div>
+  );
+};
+
+export default ExternalArticlePage;
---
+++ src/app/external/business/page.tsx
@@ -0,0 +1,9 @@
+const businessPage = () => {
+  return (
+    <div>
+      <h1>Business Page</h1>
+    </div>
+  );
+};
+
+export default businessPage;
